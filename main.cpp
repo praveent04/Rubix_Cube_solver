d06942666ba9e5f70bd137b3cd9083c3dc2e5be6
praveen <praveentiwari29004@gmail.com>
2024-06-25 01:02:32 +0530

    main modify solver added

--- main.cpp
+++ main.cpp
@@ -1,122 +1,31 @@
-
 #include <bits/stdc++.h>
-#include "Model/RubiksCube3dArray.cpp"
-#include "Model/RubiksCube1dArray.cpp"
 
+#include "Solver/DFSSolver.h"
+#include "Solver/BFSSolver.h"
+
+#include "PatternDatabases/CornerDBMaker.h"
 
 using namespace std;
 
-int main() {
-    RubiksCube3dArray object3DArray;
-    RubiksCube1dArray object1dArray;
+int main()
+{
 
+    RubiksCube3dArray cube;
+    auto shuffleMoves = cube.randomShuffleCube(13);
+    cube.print();
+    for (auto move : shuffleMoves)
+        cout << cube.getMove(move) << " ";
+    cout << "\n";
 
-    object3DArray.print();
+    DFSSolver<RubiksCube3dArray, Hash3d> solver(cube);
+    // BFSSolver<RubiksCubeBitboard, HashBitboard> solver(cube);
 
-    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
+    auto moves = solver.solve();
 
-    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
-
-
-
-
-    object3DArray.u();
-    object1dArray.u();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.l();
-    object1dArray.l();
-
-    object3DArray.print();
-    object1dArray.print();
-
-    object3DArray.f();
-    object1dArray.f();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.r();
-    object1dArray.r();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.b();
-    object1dArray.b();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.d();
-    object1dArray.d();
-
-    object3DArray.print();
-    object1dArray.print();
-
-    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
-
-    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
-
-
-
-    object3DArray.dp();
-    object1dArray.dp();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.bp();
-    object1dArray.bp();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.rp();
-    object1dArray.rp();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.fp();
-    object1dArray.fp();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.lp();
-    object1dArray.lp();
-
-    object3DArray.print();
-    object1dArray.print();
-
-
-    object3DArray.up();
-    object1dArray.up();
-
-    object3DArray.print();
-    object1dArray.print();
-
-    if (object3DArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
-
-    if (object1dArray.isSolved()) cout << "SOLVED\n\n";
-    else cout << "NOT SOLVED\n\n";
-
+    solver.rubiksCube.print();
+    for (auto move : moves)
+        cout << cube.getMove(move) << " ";
+    cout << "\n";
 
     return 0;
 }
